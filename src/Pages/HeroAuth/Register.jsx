5af84b430ebbb604fa1d65cf83328585e64cf855
philjot <philemon_opoku@yahoo.com>
2024-11-26 11:26:58 +0100

    state

--- src/Pages/HeroAuth/Register.jsx
+++ src/Pages/HeroAuth/Register.jsx
@@ -171,7 +171,7 @@ function Register() {
 
           <div className="col-span-6 sm:flex sm:items-center sm:gap-4">
             <button
-              className={`inline-block shrink-0 rounded-md border border-blue-600 bg-amber-600 px-12 py-3 text-sm font-medium text-white transition hover:bg-transparent hover:text-amber-600 focus:outline-none focus:ring active:text-blue-500 ${loading?"cursor-not-allowed opacity-55":"null"}`} disabled={loading}            >
+              className={`inline-block shrink-0 rounded-md border border-blue-600 bg-amber-600 px-12 py-3 text-sm font-medium text-white transition hover:bg-transparent hover:text-amber-600 focus:outline-none focus:ring active:text-blue-500 ${loading?"cursor-not-allowed opacity-55":""}`} disabled={loading}            >
               {loading? "loading..." : "Create an account"}
             </button>
 
